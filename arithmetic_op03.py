cbf8bf363d497ddfedabb1b7b845db963230111d
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-07 18:12:40 +0500

    03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,7 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number=56
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer=(number%10)*10+number//10
 #Print the value of the 'answer'
-
+print(answer)
 
