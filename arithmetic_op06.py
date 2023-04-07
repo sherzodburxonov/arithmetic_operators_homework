c971dd723617321d118ae4e8069b50e1d3e4454e
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-07 18:28:08 +0500

    06

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,10 @@
 #Create a variable called 'number' and assign any value of int to it.
- 
+number=int(79)
 #Divide the number by 2 and assign number.
- 
+x=number/2
 #Find the fraction of the number and assign to a variable called 'answer'.
- 
+answer=number%2/2
 #Print the answer.
- 
+print(x)
 #Print the number.
+print(answer)
